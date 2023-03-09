da07a4a87fe76c3c10feb07489419cfebc31b8c3
kuldeep-IT <rathod99244@gmail.com>
2023-03-09 15:49:18 +0530

    - algorithm - item impression on scroll

--- app/src/main/java/com/tatvic/ecommercetraining/ProductListing.java
+++ app/src/main/java/com/tatvic/ecommercetraining/ProductListing.java
@@ -1,37 +1,27 @@
 package com.tatvic.ecommercetraining;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-import static java.lang.reflect.Array.set;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.os.Parcelable;
-import android.preference.PreferenceManager;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.gson.Gson;
 import com.tatvic.ecommercetraining.adapters.PLPListAdapter;
-import com.tatvic.ecommercetraining.model.ProductModel;
 import com.tatvic.ecommercetraining.model.CategoryModel;
+import com.tatvic.ecommercetraining.model.ProductModel;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class ProductListing extends AppCompatActivity implements PLPListAdapter.MenuListClickListener, com.tatvic.ecommercetraining.adapters.PLPListAdapter.ProductClickListener, com.tatvic.ecommercetraining.adapters.PLPListAdapter.AddToCartListener, com.tatvic.ecommercetraining.adapters.PLPListAdapter.RemoveFromCartListener {
 
@@ -45,9 +35,16 @@ public class ProductListing extends AppCompatActivity implements PLPListAdapter.
     private FirebaseAnalytics mFirebaseAnalytics;
     CategoryModel categoryModel;
     Bundle product, itemAddToCart;
+    private LinearLayoutManager linearLayoutManager;
+    private boolean isFirstTime;
+    private boolean indexLastLog = true;
+    private int flag = 0;
+    private List<Bundle> arrayBundle = new ArrayList<Bundle>();
+    ;
 
-    static
-    {
+    private RecyclerView.OnScrollListener recyclerViewOnScrollListener;
+
+    static {
         itemsInCartList = new ArrayList<>();
     }
 
@@ -61,12 +58,19 @@ public class ProductListing extends AppCompatActivity implements PLPListAdapter.
         product = new Bundle();
         menuList = categoryModel.getMenus();
 
+        isFirstTime = true;
+
+
+        linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
         initRecyclerView();
+
+
         buttonCheckout = findViewById(R.id.buttonCheckout);
         buttonCheckout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(itemsInCartList == null && itemsInCartList.size() <= 0) {
+                if (itemsInCartList == null && itemsInCartList.size() <= 0) {
                     Snackbar.make(v, "Please add some items in cart", Snackbar.LENGTH_LONG)
                             .setAction("OK", new View.OnClickListener() {
                                 @Override
@@ -86,8 +90,139 @@ public class ProductListing extends AppCompatActivity implements PLPListAdapter.
     }
 
     private void initRecyclerView() {
-        RecyclerView recyclerView =  findViewById(R.id.rv_PLP);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView recyclerView = findViewById(R.id.rv_PLP);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                int visibleItemCount = linearLayoutManager.getChildCount();
+                int totalItemCount = linearLayoutManager.getItemCount();
+                int firstVisibleItemPosition = linearLayoutManager.findFirstVisibleItemPosition();
+                int lastItem = firstVisibleItemPosition + visibleItemCount - 2;
+                int lastIndexItem = firstVisibleItemPosition + visibleItemCount - 1;
+
+                Log.d("lastIndexItem", "Last Index::: " + lastIndexItem);
+
+                Log.d(
+                        "RecyclerView_Data",
+                        "onScrolled: \n " + "visibleItemCount: " + visibleItemCount + "\n" + "totalItemCount: " + totalItemCount + "\n" + "firstVisibleItemPosition: " + firstVisibleItemPosition + "\n" + "lastItem: " + lastItem
+                );
+
+                if (isFirstTime) {
+                    isFirstTime = false;
+                    flag = lastItem;
+
+                    Log.d("GA_FIRED", "onScrolled:IF");
+
+                    if (lastItem >= 0 && flag >= lastItem) {
+
+                        for (int i = 0; i <= lastItem; i++) {
+                            Bundle bundle = new Bundle();
+                            bundle.putString(FirebaseAnalytics.Param.ITEM_ID, menuList.get(i).getName());
+                            bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, menuList.get(i).getName());
+                            arrayBundle.add(i, bundle);
+                        }
+
+                        Log.d("CREATED_ARRAY", "array data::" + arrayBundle);
+
+                        Bundle viewItemListBundle = new Bundle();
+
+                        viewItemListBundle.putString(FirebaseAnalytics.Param.ITEM_LIST_ID, "list_1");
+                        viewItemListBundle.putString(FirebaseAnalytics.Param.ITEM_LIST_NAME, "FirstExecuteItem");
+                        viewItemListBundle.putParcelableArrayList(FirebaseAnalytics.Param.ITEMS, (ArrayList<? extends Parcelable>) arrayBundle);
+
+                        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.VIEW_ITEM_LIST, viewItemListBundle);
+
+                        Log.d(
+                                "GA_FIRED_COUNT",
+                                "onScrolled: First Visible " + firstVisibleItemPosition + " lastItem: " + lastItem);
+
+                        Log.d("GA_FIRED", "onScrolled:ELSEEE::: GA REPORTS GET HERE:: FIRST TIME");
+
+                    }
+                }
+                else if (flag < lastItem) {
+                    arrayBundle.clear();
+
+                    Log.d("GA_FIRED", "onScrolled:MAIN ELSEEE::: GA REPORTS GET HERE");
+                    Log.d(
+                            "GA_FIRED_COUNT",
+                            "onScrolled: First Visible " + firstVisibleItemPosition + "${lastItem} " + lastItem
+                    );
+
+
+                    for(int i=flag+1; i<=lastItem; i++){
+
+                        Log.d("LAST_INDEX", "onScrolled: First Index "+flag+" lastIndex "+lastItem);
+
+                        Log.d("I_AM_WATCHING_U", "onScrolled: "+i);
+
+                        Bundle bundle = new Bundle();
+                        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, menuList.get(i).getName());
+                        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, menuList.get(i).getName());
+                        arrayBundle.add( bundle);
+
+                    }
+                    Log.d("CREATED_ARRAY", "array data::" + arrayBundle);
+
+                    Bundle viewItemListBundle = new Bundle();
+
+                    viewItemListBundle.putString(FirebaseAnalytics.Param.ITEM_LIST_ID, "list_1");
+                    viewItemListBundle.putString(FirebaseAnalytics.Param.ITEM_LIST_NAME, "SecondScrollItem");
+                    viewItemListBundle.putParcelableArrayList(FirebaseAnalytics.Param.ITEMS, (ArrayList<? extends Parcelable>) arrayBundle);
+
+                    mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.VIEW_ITEM_LIST, viewItemListBundle);
+
+                    Log.d(
+                            "GA_FIRED_COUNT",
+                            "onScrolled: First Visible " + firstVisibleItemPosition + " lastItem: " + lastItem);
+
+                    Log.d("GA_FIRED", "onScrolled:ELSEEE::: GA REPORTS GET HERE:: FIRST TIME");
+
+                    //flag updated here
+                    flag = lastItem;
+
+                    Log.d("CREATED_ARRAY", "last ITEM else if: "+menuList.size());
+                }
+                else if(menuList.size()-1 == lastIndexItem){
+
+                    Log.d("CREATED_ARRAY", "last ITEM: ");
+
+                    if (indexLastLog == true) {
+                        indexLastLog = false;
+                        arrayBundle.clear();
+
+                        Bundle bundle = new Bundle();
+                        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, menuList.get(menuList.size()-1).getName());
+                        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, menuList.get(menuList.size()-1).getName());
+                        arrayBundle.add( bundle);
+
+                        Log.d("CREATED_ARRAY", "array data::" + arrayBundle);
+
+
+                        Bundle viewItemListBundle = new Bundle();
+
+                        viewItemListBundle.putString(FirebaseAnalytics.Param.ITEM_LIST_ID, "last_index");
+                        viewItemListBundle.putString(FirebaseAnalytics.Param.ITEM_LIST_NAME, "lastItem");
+                        viewItemListBundle.putParcelableArrayList(FirebaseAnalytics.Param.ITEMS, (ArrayList<? extends Parcelable>) arrayBundle);
+
+                        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.VIEW_ITEM_LIST, viewItemListBundle);
+
+                    }
+                }
+            }
+        });
+
+
         MainActivity mainActivity = new MainActivity();
         PLPListAdapter = new PLPListAdapter(this, menuList,
                 this, this, this, this);
@@ -102,16 +237,15 @@ public class ProductListing extends AppCompatActivity implements PLPListAdapter.
 //    }
 
 
-
     @Override
     public void onAddToCartClick(ProductModel productModel) {
-        if(itemsInCartList == null) {
+        if (itemsInCartList == null) {
 //            itemsInCartList = new ArrayList<>();
         }
         itemsInCartList.add(productModel);
         totalItemInCart = 0;
 
-        for(ProductModel m : itemsInCartList) {
+        for (ProductModel m : itemsInCartList) {
             totalItemInCart = totalItemInCart + m.getTotalInCart();
         }
         //buttonCheckout.setText("Checkout (" +totalItemInCart +") items");
@@ -119,14 +253,14 @@ public class ProductListing extends AppCompatActivity implements PLPListAdapter.
 
     @Override
     public void onUpdateCartClick(ProductModel productModel) {
-        if(itemsInCartList.contains(productModel)) {
+        if (itemsInCartList.contains(productModel)) {
             int index = itemsInCartList.indexOf(productModel);
             itemsInCartList.remove(index);
             itemsInCartList.add(index, productModel);
 
             totalItemInCart = 0;
 
-            for(ProductModel m : itemsInCartList) {
+            for (ProductModel m : itemsInCartList) {
                 totalItemInCart = totalItemInCart + m.getTotalInCart();
             }
             //buttonCheckout.setText("Checkout (" +totalItemInCart +") items");
@@ -135,11 +269,11 @@ public class ProductListing extends AppCompatActivity implements PLPListAdapter.
 
     @Override
     public void onRemoveFromCartClick(ProductModel productModel) {
-        if(itemsInCartList.contains(productModel)) {
+        if (itemsInCartList.contains(productModel)) {
             itemsInCartList.remove(productModel);
             totalItemInCart = 0;
 
-            for(ProductModel m : itemsInCartList) {
+            for (ProductModel m : itemsInCartList) {
                 totalItemInCart = totalItemInCart + m.getTotalInCart();
             }
             //buttonCheckout.setText("Checkout (" +totalItemInCart +") items");
@@ -150,7 +284,7 @@ public class ProductListing extends AppCompatActivity implements PLPListAdapter.
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == 1000 && resultCode == Activity.RESULT_OK) {
+        if (requestCode == 1000 && resultCode == Activity.RESULT_OK) {
             //
             finish();
         }
@@ -228,4 +362,6 @@ public class ProductListing extends AppCompatActivity implements PLPListAdapter.
 
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.REMOVE_FROM_CART, removeCartParams);
     }
+
+
 }
