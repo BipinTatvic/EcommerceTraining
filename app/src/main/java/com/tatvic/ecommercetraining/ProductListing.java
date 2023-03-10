c0153d7be4e5703f012805583954785d74ae4ac7
kuldeep-IT <rathod99244@gmail.com>
2023-03-10 17:52:20 +0530

    - algorithm2 - item impression on scroll

--- app/src/main/java/com/tatvic/ecommercetraining/ProductListing.java
+++ app/src/main/java/com/tatvic/ecommercetraining/ProductListing.java
@@ -195,8 +195,6 @@ public class ProductListing extends AppCompatActivity implements PLPListAdapter.
                 }
                 else if(menuList.size()-1 == lastIndexItem){
 
-                    Log.d("CREATED_ARRAY", "last ITEM: ");
-
                     if (indexLastLog == true) {
                         indexLastLog = false;
                         arrayBundle.clear();
